47f997073ab317e103df6a3decc7d0aa3f11448f
Anihr <151598461+Anihr@users.noreply.github.com>
2024-01-09 17:23:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 22776253
 
 API_HASH = os.environ.get("API_HASH", "75360401e5b9c37da380fe04e2fe244c)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6816882157:AAGkQyRd-KVdbUim49FX9GVfAWNZ7SEGXCU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6816882157:AAEc6ppijYE56osR4tWgQb64bnW434B-GlY")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
